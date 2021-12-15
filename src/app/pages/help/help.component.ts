6dde670c175ea441e670adff44e3e0765eadb7a5
UchaBokeria <ucha1bokeria@gmail.com>
2021-12-15 13:48:37 -0800

    x

--- src/app/pages/help/help.component.ts
+++ src/app/pages/help/help.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./help.component.scss']
 })
 export class HelpComponent implements OnInit {
-
+  faq = [
+    {
+      id: 1,
+      question: "??",
+      answer: "!!"
+    },
+    {
+      id: 2,
+      question: "??",
+      answer: "!!"
+    },
+    {
+      id: 3,
+      question: "??",
+      answer: "!!"
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
